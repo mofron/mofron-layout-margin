7ca7496f4bc1034e273e46a3ff2c8280dfbb155d
simpart <simpart>
2018-07-25 14:53:21 +0000

    supprted prmmap

--- index.js
+++ index.js
@@ -9,38 +9,8 @@ mofron.layout.Margin = class extends mofron.Layout {
         try {
             super();
             this.name('Margin');
+            this.prmMap('type', 'value');
             this.prmOpt(po, p2);
-            
-            this.getParam().check(
-                (tp) => {
-                    try {
-                        if (undefined === tp) {
-                            return '';
-                        }
-                        if ( ('string' != (typeof tp)) ||
-                             ( (''       != tp) &&
-                               ('top'    != tp) &&
-                               ('right'  != tp) &&
-                               ('bottom' != tp) &&
-                               ('left'   != tp) ) ) {
-                            throw new Error('invalid parameter');
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-                (val) => {
-                    try {
-                        if ( ('number' !== typeof val) && ('string' !== typeof val) ) {
-                            throw new Error('invalid parameter');
-                        }
-                    } catch (e) {
-                        console.error(e.stack);
-                        throw e;
-                    }
-                },
-            );
         } catch (e) {
             console.error(e.stack);
             throw e;
@@ -49,16 +19,49 @@ mofron.layout.Margin = class extends mofron.Layout {
     
     contents (idx, tgt) {
         try {
-            let mg   = 'margin';
-            let type = this.value()[0];
-            let val  = this.value()[1];
-            
-            if ('' !== type) {
-                mg += '-' + type;
-            }
+            let mg     = (null === this.type()) ? 'margin' : 'margin' + '-' + this.type();
             let setmgn = {};
-            setmgn[mg] = ('number' !== typeof val) ?  val : val + 'px';
+            setmgn[mg] = ('number' !== typeof this.value()) ?  this.value() : this.value() + 'px';
             tgt.adom().style(setmgn);
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    type (prm) {
+        try {
+            if (undefined === prm) {
+                 /* getter */
+                 return (undefined === this.m_type) ? null : this.m_type;
+            }
+            /* setter */
+            if ( ('string' !== typeof prm) ||
+                 ( ('top'    !== prm) &&
+                   ('right'  !== prm) &&
+                   ('bottom' !== prm) &&
+                   ('left'   !== prm) ) ) {
+                throw new Error('invalid parameter');
+            }
+            
+            this.m_type = prm;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    value (prm) {
+        try {
+            if (undefined === prm) {
+                 /* getter */
+                 return (undefined === this.m_value) ? null : this.m_value;
+            }
+            /* setter */
+            if (('string' !== typeof prm) && ('number' !== typeof prm))  {
+                throw new Error('invalid parameter');
+            }
+            this.m_value = prm;
         } catch (e) {
             console.error(e.stack);
             throw e;
